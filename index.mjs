53b7ff2996fb346c3a95d77cc40f107314814769
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-09-07 12:47:14 +0530

    read file using createReadStream API

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
+import readline from "node:readline";
 
 console.log(process.argv);
 
@@ -12,14 +13,70 @@ const argument = process.argv[2];
 const fileName = process.argv[3];
 const filePath = path.join(__dirname, fileName);
 
-fs.readFile(filePath, (err, data) => {
-  if (err) {
-    console.error(`Error reading file: ${err.message}`);
-    return;
-  }
+let lines = 0;
+let bytes = 0;
+let words = 0;
+
+// fs.readFile(filePath, (err, data) => {
+//   if (err) {
+//     console.error(`Error reading file: ${err.message}`);
+//     return;
+//   }
+//   switch (argument) {
+//     case "-c":
+//       console.log(data.length);
+//       return;
+//     default:
+//       console.error("Invalid argument");
+//       return;
+//   }
+// });
+
+// const rl = readline.createInterface({
+//   input: fs.createReadStream(filePath),
+//   output: process.stdout,
+//   terminal: false,
+// });
+
+// rl.on("line", (line) => {
+//   lines++;
+//   bytes += Buffer.byteLength(line);
+//   words += line.split(/\s+/).length;
+// });
+
+// rl.on("close", () => {
+//   switch (argument) {
+//     case "-c":
+//       console.log(bytes);
+//       return;
+//     default:
+//       console.error("Invalid argument");
+//       return;
+//   }
+// });
+
+const reader = fs.createReadStream(filePath);
+
+reader.on("data", (chunk) => {
+  lines += chunk.toString().match(/\n/g)?.length || 0;
+  bytes += chunk.length;
+  words += chunk.toString().split(/\s+/).length;
+});
+
+reader.on("error", (error) => {
+  console.error(`Error reading file: ${error.message}`);
+});
+
+reader.on("end", () => {
   switch (argument) {
     case "-c":
-      console.log(data.length);
+      console.log(bytes);
+      return;
+    case "-l":
+      console.log(lines);
+      return;
+    case "-w":
+      console.log(words);
       return;
     default:
       console.error("Invalid argument");
